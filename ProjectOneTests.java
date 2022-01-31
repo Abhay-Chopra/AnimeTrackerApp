b907df8463e60dca6e5b65e5748e478d1d86b4e8
Abhay Chopra <abhay.chopra1@ucalgary.ca>
2022-01-31 12:04:13 -0700

    Initial Project One Tests.

---
+++ ProjectOneTests.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Abhay Chopra, Brandon Greene
+ * @version 1.0
+ * Tutorial: T06
+ */
+class ProjectOneTests {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
