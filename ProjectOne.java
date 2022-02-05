834bd21732f19c104652eb5fbc19ebf52378d17e
Brandon Greene <brandongreene@mymhc.ca>
2022-02-05 09:54:40 -0700

    added command list, functions, and loop

--- ProjectOne.java
+++ ProjectOne.java
@@ -7,9 +7,57 @@ import java.util.Scanner;
  * Tutorial: T06 (w/ Amir)
  * Objective: Software that tracks the user's top streamed anime category
  */
-public class ProjectOne {
-    public static void main(String[] args) {
+public class ProjectOne
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String[] cmds = {"help", "add", "remove", "exit"};
+        boolean notQuit = true;
+
+        System.out.println("Welcome, please input one of the following commands!");
+
+        for(String x : cmds)
+        {
+            System.out.println(x);
+        }
+
+        do
+        {
+            String input = scan.nextLine();
+            switch (input)
+            {
+                case "help" -> help();
+                case "add" -> add();
+                case "remove" -> remove();
+                case "exit" -> notQuit = setQuit();
+                default -> printError();
+            }
+        }
+        while(notQuit);
+
         getUserInput();
+    }
+
+    private static void printError()
+    {
+    }
+
+    private static boolean setQuit()
+    {
+        return true;
+    }
+
+    private static void remove()
+    {
+    }
+
+    private static void add()
+    {
+    }
+
+    private static void help()
+    {
     }
 
     /**
