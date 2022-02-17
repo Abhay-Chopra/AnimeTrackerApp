8be4d179e5be3c69f9de857dbdba0c26469108a1
Abhay Chopra <abhay.chopra1@ucalgary.ca>
2022-02-17 16:45:27 -0700

    Added utility functions: cleanNextIn and printInterface.

--- ProjectOne.java
+++ ProjectOne.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -29,23 +28,24 @@ public class ProjectOne
         System.out.println("Brandon's And Abhay's Anime List");
         System.out.println("-----------------------------");
 
-        System.out.println("(Select A Command With The Number (ie: 5 for Help))");
+        /*System.out.println("(Select A Command With The Number (ie: 5 for Help))");
         //Display our list of commands
         for(int i = 0; i < cmds.length; i++)
-            System.out.println(i+1 + ") " + cmds[i]);
+            System.out.println(i+1 + ") " + cmds[i]);*/
 
         //Make a scanner to get user input
         Scanner scan = new Scanner(System.in);
         do
         {
+            printInterface(cmds);
             int input = scan.nextInt();
             switch (input)
             {
-                case 1 -> help(cmds);
-                case 2 -> anime = getUserInput();
-                case 3 -> remove();
-                case 4 -> notQuit = setQuit(scan);
-                case 5 -> displayInformation();
+                case 1 -> anime = getUserInput(scan);
+                case 2 -> remove();
+                case 3 -> notQuit = setQuit(scan);
+                case 4 -> displayInformation();
+                case 5 -> help(cmds);
                 default -> printError();
             }
         }
@@ -63,8 +63,8 @@ public class ProjectOne
 
     private static boolean setQuit(Scanner scan)
     {
-        System.out.println("Are you sure you wish to quit? (Y, N)");
-        return !scan.nextLine().equals("Y");
+        System.out.println("Are you sure you wish to quit? (Yes:1, No:0)");
+        return !(scan.nextInt() == 1);
     }
 
     private static void remove()
@@ -95,12 +95,12 @@ public class ProjectOne
      *
      * @return HasMap<String, ArrayList<String>>
      */
-    public static HashMap<String, HashMap<ArrayList<String>, Integer>> getUserInput() {
-        // get some type of input
-        Scanner scan = new Scanner(System.in);
+    public static HashMap<String, HashMap<ArrayList<String>, Integer>> getUserInput(Scanner scan) {
         // getting anime name
         System.out.println("What is the anime name?");
-        String animeName = scan.nextLine().strip().toLowerCase();
+        scan.nextLine();
+        // sanitizing input function
+        String animeName = cleanNextIn(scan);
 
         // bringing vars into scope`
         ArrayList<String> genresList = new ArrayList<>();
@@ -108,7 +108,7 @@ public class ProjectOne
 
         // getting the genre corresponding to the anime
         System.out.println("Please enter the genres of this anime, ie, \"romance,action\" (exit to quit): ");
-        String genresInput = scan.nextLine().strip().toLowerCase();
+        String genresInput = cleanNextIn(scan);
         String[] genres = genresInput.split(",");
         Collections.addAll(genresList, genres);
 
@@ -116,13 +116,36 @@ public class ProjectOne
         System.out.println("How many episodes did you watch of " + animeName + "?");
         episodesWatched = Integer.parseInt(scan.nextLine());
 
-        // creating nested HashMaps
+        // setting up nested hash structure
         HashMap<String, HashMap<ArrayList<String>, Integer>> outerHash = new HashMap<>();
         HashMap<ArrayList<String>, Integer> innerHash = new HashMap<>();
-
         innerHash.put(genresList, episodesWatched);
         outerHash.put(animeName, innerHash);
 
+        // This is strictly for testing | IGNORE THIS
+        System.out.printf("\nStored Anime name: %s\nStored Genres: %s\nStored #watched: %s\n\n",
+                                               animeName, Arrays.toString(genres), episodesWatched);
+
         return outerHash;
+    }
+
+    /**
+     * Gets and cleans the next user input
+     * @param scan Scanner object to get String input
+     * @return Cleaned String
+     */
+    private static String cleanNextIn(Scanner scan) {
+        return scan.nextLine().toLowerCase().strip();
+    }
+
+    /**
+     * Prints out the interface, ie, all the different options
+     * @param cmds Array containing all the commands
+     */
+    private static void printInterface(String[] cmds){
+        System.out.println("(Select A Command With The Number (ie: 5 for Help))");
+        //Display our list of commands
+        for(int i = 0; i < cmds.length; i++)
+            System.out.println(i+1 + ") " + cmds[i]);
     }
 }
