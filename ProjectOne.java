1b1cc2eab99c8a91c9898301c82070f172da3581
Abhay Chopra <abhay.chopra1@ucalgary.ca>
2022-02-22 22:21:00 -0700

    Added function (output) to determine which anime are currently being tracked.

--- ProjectOne.java
+++ ProjectOne.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  * @author Abhay Chopra, Brandon Greene
@@ -6,15 +8,13 @@ import java.util.*;
  * Tutorial: T06 (w/ Amir)
  * Objective: Software that tracks the user's top streamed anime category
  */
-public class ProjectOne
-{
+public class ProjectOne {
     //Episode time in minutes
     private static final int AVG_EP_TIME = 23;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         boolean notQuit = true;
-        String[] cmds = {"Add", "Remove", "Info", "Help", "Output Commands" ,"Exit"};
+        String[] cmds = {"Add", "Remove", "Info", "Help", "Output Commands", "Exit Program"};
 
         //Structures for holding our data
         ArrayList<String> animeList = new ArrayList<>();
@@ -33,23 +33,21 @@ public class ProjectOne
         Scanner scan = new Scanner(System.in);
 
         //Loop for main functionality of the program
-        do
-        {
+        do {
             printInterface(cmds);
             int input = scan.nextInt();
             scan.nextLine();
-            switch (input)
-            {
+            switch (input) {
                 case 1 -> getUserInput(scan, animeList, studioList, animeGenre, animeEpisodeCount, animeStudio, animeScore);
                 case 2 -> remove(animeList);
                 case 3 -> displayInformation();
                 case 4 -> help();
-                case 5 -> outputCases(scan);
+                case 5 -> outputCases(scan, animeList, studioList, animeGenre, animeEpisodeCount, animeStudio, animeScore);
                 case 6 -> notQuit = setQuit(scan);
                 default -> printError();
             }
         }
-        while(notQuit);
+        while (notQuit);
 
         //Only get here upon user choosing to exit
         System.out.println("-------------------------------------");
@@ -57,26 +55,22 @@ public class ProjectOne
         System.out.println("-------------------------------------");
     }
 
-    private static void displayInformation()
-    {
+    private static void displayInformation() {
 
     }
 
-    private static void printError()
-    {
+    private static void printError() {
         System.out.println("Error reading command, please enter command by integer again!");
     }
 
-    private static boolean setQuit(Scanner scan)
-    {
+    private static boolean setQuit(Scanner scan) {
         System.out.println("Are you sure you wish to quit? (Yes:1, No:0)");
         return !(scan.nextInt() == 1);
     }
 
-    private static void remove(ArrayList<String> animeList)
-    {
+    private static void remove(ArrayList<String> animeList) {
         System.out.println("Which of the following would you like to remove? ");
-        for(int i = 0; i < animeList.size(); i++){
+        for (int i = 0; i < animeList.size(); i++) {
             System.out.printf("%s ", i);
         }
     }
@@ -84,28 +78,28 @@ public class ProjectOne
     /**
      * Displays help for each command usage
      */
-    private static void help()
-    {
+    private static void help() {
         System.out.print(
                 """
-                -----------------------------------------------------------
-                add -> add an anime to the list (Name, Genre, # of Episodes)
-                remove -> remove an anime via name from the list
-                exit -> exit the program
-                help -> displays help for commands
-                -----------------------------------------------------------
-                """);
+                        -----------------------------------------------------------
+                        add -> add an anime to the list (Name, Genre, # of Episodes)
+                        remove -> remove an anime via name from the list
+                        exit -> exit the program
+                        help -> displays help for commands
+                        -----------------------------------------------------------
+                        """);
     }
 
     /**
      * The main input function
-     * @param scan scanner for getting the input
-     * @param animeList the main list of stored anime
-     * @param studioList the main list of stored studios
-     * @param animeGenre the linked anime/genre hashmap
+     *
+     * @param scan              scanner for getting the input
+     * @param animeList         the main list of stored anime
+     * @param studioList        the main list of stored studios
+     * @param animeGenre        the linked anime/genre hashmap
      * @param animeEpisodeCount the linked anime/episodeCount hashmap
-     * @param animeStudio tying anime and studios together
-     * @param animeScore giving anime a 1-10 score
+     * @param animeStudio       tying anime and studios together
+     * @param animeScore        giving anime a 1-10 score
      */
     public static void getUserInput(Scanner scan,
                                     ArrayList<String> animeList,
@@ -113,8 +107,7 @@ public class ProjectOne
                                     HashMap<String, ArrayList<String>> animeGenre,
                                     HashMap<String, Integer> animeEpisodeCount,
                                     HashMap<String, String> animeStudio,
-                                    HashMap<String, Double> animeScore)
-    {
+                                    HashMap<String, Double> animeScore) {
         ArrayList<String> genres = new ArrayList<>();
 
         System.out.println("What anime would you like to add?");
@@ -128,13 +121,12 @@ public class ProjectOne
 
         System.out.println("What genres does this anime fall under (ie: romance, action [done when finished])?");
         String newGenre = scan.nextLine();
-        do
-        {
-            if(!newGenre.isEmpty() || newGenre.trim().length() > 0)
+        do {
+            if (!newGenre.isEmpty() || newGenre.trim().length() > 0)
                 genres.add(newGenre);
-           System.out.println("Another genre? [done when finished]");
-           newGenre = scan.nextLine();
-        }while(!newGenre.equals("done"));
+            System.out.println("Another genre? [done when finished]");
+            newGenre = scan.nextLine();
+        } while (!newGenre.equals("done"));
         animeGenre.put(newAnime, genres);
 
         System.out.println("How many episode have you watched?");
@@ -150,33 +142,37 @@ public class ProjectOne
 
     /**
      * Prints out the interface, ie, all the different options
+     *
      * @param cmds Array containing all the commands
      */
-    private static void printInterface(String[] cmds)
-    {
+    private static void printInterface(String[] cmds) {
         System.out.println("Select A Command With The Number:");
 
         //Display our list of commands
-        for(int i = 0; i < cmds.length; i++)
-            System.out.println(i+1 + ") " + cmds[i]);
+        for (int i = 0; i < cmds.length; i++)
+            System.out.println(i + 1 + ") " + cmds[i]);
     }
 
     /**
      * Function for all the handling of output commands
      */
-    private static void outputCases(Scanner scan){
-        String [] outputCmds = {"Print All Anime Tracked", "Total Watch Time", "Top Streamed Anime",
-                                "Top Streamed Genre", "Give Anime Name" ,"Help", "Exit to Main Menu"};
+    private static void outputCases(Scanner scan,
+                                    ArrayList<String> animeList,
+                                    ArrayList<String> studioList,
+                                    HashMap<String, ArrayList<String>> animeGenre,
+                                    HashMap<String, Integer> animeEpisodeCount,
+                                    HashMap<String, String> animeStudio,
+                                    HashMap<String, Double> animeScore) {
+        String[] outputCmds = {"Print All Anime Tracked", "Total Watch Time", "Top Streamed Anime",
+                "Top Streamed Genre", "Give Anime Name", "Help", "Exit to Main Menu"};
         boolean notQuit = true;
-        // looping for the output commands interface
-        do
-        {
+        //Looping for the output commands interface
+        do {
             printInterface(outputCmds);
             int input = scan.nextInt();
             scan.nextLine();
-            switch (input)
-            {
-                case 1 -> allAnimeTracked();
+            switch (input) {
+                case 1 -> allAnimeTracked(animeList);
                 case 2 -> totalWatchTime();
                 case 3 -> topStreamedAnime();
                 case 4 -> topStreamedGenre();
@@ -186,28 +182,40 @@ public class ProjectOne
                 default -> printError();
             }
         }
-        while(notQuit);
-    }
-
-    private static void giveAnimeName() {
-    }
-
-    private static void topStreamedGenre() {
+        while (notQuit);
     }
 
     private static void topStreamedAnime() {
     }
 
+    private static void topStreamedGenre() {
+    }
+
     private static void totalWatchTime() {
     }
 
+    private static void giveAnimeName() {
+    }
+
     private static boolean exitToMain(Scanner scan) {
-        return true;
+        System.out.println("Are you sure you want to quit back to the Main Menu? (Yes:1, No:0)");
+        return !(scan.nextInt() == 1);
+    }
+
+    private static void allAnimeTracked(ArrayList<String> animeList) {
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("The anime(s) currently tracked:");
+        if (animeList.size() == 0) {
+            System.out.println("No anime currently being tracked!");
+        } else {
+            for (int i = 0; i < animeList.size(); i++) {
+                System.out.println(i + 1 + ") " + animeList.get(i));
+            }
+        }
+        System.out.println("-----------------------------------------------------------");
     }
 
     private static void helpOutputCommands() {
     }
 
-    private static void allAnimeTracked() {
-    }
 }
