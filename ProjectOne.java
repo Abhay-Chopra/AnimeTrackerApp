3fef2e187a10d815247898c2bd0db6ac9e5d671a
Abhay Chopra <abhay.chopra1@ucalgary.ca>
2022-02-22 23:30:01 -0700

    Added output function to determine and print the top streamed anime.

--- ProjectOne.java
+++ ProjectOne.java
@@ -85,7 +85,7 @@ public class ProjectOne {
                         add -> add an anime to the list (Name, Genre, # of Episodes)
                         remove -> remove an anime via name from the list
                         output commands -> displays menu with all possible output commands
-                        
+                                                
                         exit -> exit the program
                         help -> displays help for commands
                         -----------------------------------------------------------
@@ -111,7 +111,7 @@ public class ProjectOne {
                                     HashMap<String, String> animeStudio,
                                     HashMap<String, Double> animeScore) {
         ArrayList<String> genres = new ArrayList<>();
-
+        //TODO Add a method to reject same anime being added twice
         System.out.println("What anime would you like to add?");
         String newAnime = scan.nextLine();
         animeList.add(newAnime);
@@ -130,15 +130,15 @@ public class ProjectOne {
             newGenre = scan.nextLine();
         } while (!newGenre.equals("done"));
         animeGenre.put(newAnime, genres);
-
+        //TODO Add a way to check if positive integer value
         System.out.println("How many episode have you watched?");
         animeEpisodeCount.put(newAnime, scan.nextInt());
 
         System.out.println("What rating would you give this anime (1-10)");
         animeScore.put(newAnime, scan.nextDouble());
-
-        System.out.println("\nCompleted adding " + newAnime + " to the list!");
-
+        System.out.println("\n-----------------------------------------------------------");
+        System.out.println("Completed adding " + newAnime + " to the list!");
+        System.out.println("-----------------------------------------------------------\n");
         scan.nextLine();
     }
 
@@ -175,8 +175,8 @@ public class ProjectOne {
             scan.nextLine();
             switch (input) {
                 case 1 -> allAnimeTracked(animeList);
-                case 2 -> totalWatchTime();
-                case 3 -> topStreamedAnime();
+                case 2 -> totalWatchTime(animeEpisodeCount);
+                case 3 -> topStreamedAnime(animeList, animeEpisodeCount);
                 case 4 -> topStreamedGenre();
                 case 5 -> giveAnimeName();
                 case 6 -> helpOutputCommands();
@@ -187,23 +187,63 @@ public class ProjectOne {
         while (notQuit);
     }
 
-    private static void topStreamedAnime() {
+    /**
+     * Determines the top streamed anime based on total watch time of each anime
+     *
+     * @param animeList         Arraylist containing all anime being tracked
+     * @param animeEpisodeCount //TODO Javadoc
+     */
+    private static void topStreamedAnime(ArrayList<String> animeList, HashMap<String, Integer> animeEpisodeCount) {
+        if (animeList.size() > 0) {
+            String topStreamedAnime = animeList.get(0);
+            Integer episodeCount = -1;
+            for (String anime : animeList) {
+                if (animeEpisodeCount.get(anime) > episodeCount) {
+                    topStreamedAnime = anime;
+                    episodeCount = animeEpisodeCount.get(anime);
+                }
+            }
+            System.out.println("-----------------------------------------------------------");
+            System.out.printf("Your current top streamed anime is: %s\nEstimated watch time: %s minutes\n", topStreamedAnime, episodeCount * AVG_EP_TIME);
+            System.out.println("-----------------------------------------------------------");
+        } else {
+            System.out.println("-----------------------------------------------------------");
+            System.out.println("No anime currently being tracked!");
+            System.out.println("-----------------------------------------------------------");
+        }
     }
 
+    /**
+     *
+     */
     private static void topStreamedGenre() {
     }
 
-    private static void totalWatchTime() {
+    /**
+     * @param animeEpisodeCount
+     */
+    private static void totalWatchTime(HashMap<String, Integer> animeEpisodeCount) {
+        System.out.println(animeEpisodeCount);
     }
 
+    /**
+     *
+     */
     private static void giveAnimeName() {
     }
 
+    /**
+     * @param scan
+     * @return
+     */
     private static boolean exitToMain(Scanner scan) {
         System.out.println("Are you sure you want to quit back to the Main Menu? (Yes:1, No:0)");
         return !(scan.nextInt() == 1);
     }
 
+    /**
+     * @param animeList
+     */
     private static void allAnimeTracked(ArrayList<String> animeList) {
         System.out.println("-----------------------------------------------------------");
         System.out.println("The anime(s) currently tracked:");
@@ -226,7 +266,7 @@ public class ProjectOne {
                         Top Streamed Anime -> Displays your top streamed anime, determined by watch time
                         Top Streamed Genre -> Displays top anime genre, determined by watch time
                         Anime By Genre -> Given a genre, provides an anime (from current tracked anime)
-                        
+                                                
                         exit -> exit to main menu
                         help -> displays help for commands
                         -----------------------------------------------------------
