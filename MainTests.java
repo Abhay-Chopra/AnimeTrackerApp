3bdae5338b74324841753bba330e1fd45e5da6ce
Brandon Greene <brandongreene@mymhc.ca>
2022-02-25 14:42:56 -0700

    Setup test containers

--- MainTests.java
+++ MainTests.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.*;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,30 +12,58 @@ import java.util.HashMap;
 class MainTests
 {
     ArrayList<String> emptyAnimeList;
-    HashMap<String, Integer> noAnimeHash;
+    HashMap<String, Integer> noAnimeEpisodes;
+    HashMap<String, ArrayList<String>> noAnimeGenres;
+
     ArrayList<String> oneEntryAnimeList;
-    HashMap<String, Integer> oneAnimeHash;
+    HashMap<String, Integer> oneAnimeEpisodes;
+    HashMap<String, ArrayList<String>> oneAnimeGenre;
+
     ArrayList<String> multipleAnimeList;
-    HashMap<String, Integer> multipleAnimeHash;
+    HashMap<String, Integer> multipleAnimeEpisodes;
+    HashMap<String, ArrayList<String>> multipleAnimeGenre;
+
+
     @BeforeEach
     private void setupEmpty(){
         emptyAnimeList = new ArrayList<>(){};
-        noAnimeHash = new HashMap<>();
+        noAnimeEpisodes = new HashMap<>();
+        noAnimeGenres = new HashMap<>();
     }
+
     @BeforeEach
     private void setupOneEntry(){
+        int episodes = 72;
+        String anime = "ATTACK ON TITAN";
+        ArrayList<String> genres = new ArrayList<>();
+        genres.add("action");
+        genres.add("drama");
         oneEntryAnimeList = new ArrayList<>(){};
-        oneEntryAnimeList.add("ATTACK ON TITAN");
-        oneAnimeHash = new HashMap<>();
-        oneAnimeHash.put("ATTACK ON TITAN", 12);
+        oneEntryAnimeList.add(anime);
+        oneAnimeEpisodes = new HashMap<>();
+        oneAnimeEpisodes.put(anime, episodes);
+        oneAnimeGenre.put(anime, genres);
     }
+
     @BeforeEach
     private void setupMultiple(){
+        String animeOne = "ATTACK ON TITAN";
+        String animeTwo = "DEMON SLAYER";
+        int oneEpisodes = 72;
+        int twoEpisodes = 44;
+        ArrayList<String> oneGenre = new ArrayList<>();
+        ArrayList<String> twoGenre = new ArrayList<>();
+        oneGenre.add("action");
+        oneGenre.add("drama");
+        twoGenre.add("action");
+        twoGenre.add("horror");
         multipleAnimeList = new ArrayList<>(){};
-        multipleAnimeList.add("ATTACK ON TITAN");
-        multipleAnimeList.add("DEMON SLAYER");
-        multipleAnimeHash.put("ATTACK ON TITAN", 12);
-        multipleAnimeHash.put("DEMON SLAYER", 12);
+        multipleAnimeList.add(animeOne);
+        multipleAnimeList.add(animeTwo);
+        multipleAnimeEpisodes.put(animeOne, oneEpisodes);
+        multipleAnimeEpisodes.put(animeTwo, twoEpisodes);
+        multipleAnimeGenre.put(animeOne, oneGenre);
+        multipleAnimeGenre.put(animeTwo, twoGenre);
     }
     //Tests for topStreamedAnime function
     @Test
