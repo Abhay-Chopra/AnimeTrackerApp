7b56e01a897d1581d591af1cfab87ef039a041f6
Abhay Chopra <abhay.chopra1@ucalgary.ca>
2022-04-02 15:42:26 -0600

    Added barebones setup of GUI

--- src/main/java/animelist/app/MainController.java
+++ src/main/java/animelist/app/MainController.java
@@ -2,10 +2,53 @@ package animelist.app;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 
 public class MainController {
     @FXML
     void alert(ActionEvent event) {
+
+    }
+
+    @FXML
+    private Button addAnime;
+
+    @FXML
+    private ComboBox<?> animeComboBox;
+
+    @FXML
+    private TextArea animeInfo;
+
+    @FXML
+    private Button deleteAnime;
+
+    @FXML
+    private TextArea outputArea;
+
+    @FXML
+    void getAllStudioTracked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void getSortedRating(ActionEvent event) {
+
+    }
+
+    @FXML
+    void outputTopStreamedAnime(ActionEvent event) {
+
+    }
+
+    @FXML
+    void topStreamedGenre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void totalWatchTime(ActionEvent event) {
 
     }
 }
